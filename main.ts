5d58eb9589b6fb674c1b8c9539e16da9062d6cb1
ZionTk6k <68042118+ZionTk6k@users.noreply.github.com>
2020-07-09 14:12:16 +1100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
-})
+for (let index = 0; index < 2; index++) {
+    basic.showString("bonjour")
+    basic.pause(500)
+    basic.showNumber(10853)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+}
